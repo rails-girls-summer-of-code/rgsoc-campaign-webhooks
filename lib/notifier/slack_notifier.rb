632d8c97ac89c3d2d7b5126a56964daa56e1392a
Sebastian Gräßl <sebastian@validcode.me>
2014-04-05 20:16:12 +0200

    add slack notifier

---
+++ lib/notifier/slack_notifier.rb
@@ -0,0 +1,44 @@
+require 'json'
+
+class SlackNotifier < Notifier::Base
+  attr_reader :account, :token, :channel
+
+
+  def initialize
+    super
+    @account = settings.slack["account"]
+    @token   = settings.slack["token"]
+    @channel = settings.slack["channel"]
+  end
+
+  def hook_url
+    @hook_url ||= "https://#{account}.slack.com/services/hooks/incoming-webhook?token=#{token}"
+  end
+
+  def uri
+    @uri ||= URI.parse(hook_url)
+  end
+
+  def connection
+    http             = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl     = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    http
+  end
+
+  def post(data)
+    request      = Net::HTTP::Post.new(uri.request_uri)
+    request.body = "payload=#{data.to_json}"
+    connection.request(request)
+  end
+
+  def notify(msg)
+    data = {
+      channel: channel,
+      text:    msg
+    }
+    post(data)
+  end
+
+end
---
+++ spec/lib/notifier/slack_notifier_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../../spec_helper.rb'
+require_relative '../../../lib/notifier'
+require_relative '../../../lib/notifier/slack_notifier'
+
+describe SlackNotifier do
+  it "has a notify method" do
+    subject.respond_to?(:notify).should be(true)
+  end
+
+  describe '#notify' do
+    it "sends a webhook to slack" do
+      request = stub_request(:post, subject.hook_url)
+
+      subject.notify("Test")
+
+      request.should have_been_made
+    end
+  end
+end
